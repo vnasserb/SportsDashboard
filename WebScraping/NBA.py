416416d1939b63a3c0cdee475369550b4072dd78
vnasserb <83405997+vnasserb@users.noreply.github.com>
2023-11-30 16:19:25 -0300

    Update NBA.py

--- WebScraping/NBA.py
+++ WebScraping/NBA.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
+from datetime import datetime
 
 def getConferenceStandings(season):
   def tableToJSON(rows):
