fa37ef9cc7719c583283adcc0dcb0bcdcc1a97c2
vnasserb <83405997+vnasserb@users.noreply.github.com>
2023-11-30 15:18:49 -0300

    Update 1_HomePage.py

--- Streamlit/1_HomePage.py
+++ Streamlit/1_HomePage.py
@@ -18,3 +18,10 @@ from NBA import *
 from Styles import *
 
 st.title("Sports Dashboard")
+
+st.text("**Use this cool dashboard to check information about many sports**)
+st.text("**Currently, we support soccer, basketball and Ice Hockey**)
+
+st.header("Soccer")
+writeColumns('image', 'https://cdn.ssref.net/req/202311071/tlogo/fb/9.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/12.png',
+            'https://cdn.ssref.net/req/202311071/tlogo/fb/20.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/24.png')
