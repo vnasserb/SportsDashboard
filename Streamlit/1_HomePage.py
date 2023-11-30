29bc4f329dfb25de46ec85c3bfef2c144e1989a6
vnasserb <83405997+vnasserb@users.noreply.github.com>
2023-11-30 13:58:26 -0300

    Update 1_HomePage.py

--- Streamlit/1_HomePage.py
+++ Streamlit/1_HomePage.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import streamlit as st
+import json
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.title("Sports Dashboard")
