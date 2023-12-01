005dee542c3531883dbf0520e466c3b842726cc1
vnasserb <83405997+vnasserb@users.noreply.github.com>
2023-11-30 22:01:46 -0300

    Refactor: Add markdowns

--- Streamlit/1_HomePage.py
+++ Streamlit/1_HomePage.py
@@ -23,7 +23,8 @@ st.write("**Use this cool dashboard to check information about many sports**")
 st.write("**Currently, we support soccer, basketball and Ice Hockey**")
 
 st.header("Soccer")
-st.text("This dashboard has information from many leagues around the world. Some of them are listed here.")
+st.write("This dashboard has information from many leagues around the world.")
+st.write("Some of them are listed here.")
 writeColumns('image', 'https://cdn.ssref.net/req/202311071/tlogo/fb/9.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/12.png',
             'https://cdn.ssref.net/req/202311071/tlogo/fb/20.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/11.png', 
              'https://cdn.ssref.net/req/202311071/tlogo/fb/13.png')
@@ -36,12 +37,19 @@ writeColumns('image', 'https://cdn.ssref.net/req/202311071/tlogo/fb/55.png', 'ht
             'https://cdn.ssref.net/req/202311071/tlogo/fb/62.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/70.png',
             'https://cdn.ssref.net/req/202311071/tlogo/fb/82.png')
 
+st.markdown("---")
+
 st.header("Basketball")
-st.text("Currently, this dashboard only has information about the NBA. More leagues will be available in the future.")
+st.write("Currently, this dashboard only has information about the NBA.")
+st.write("More leagues will be available in the future.")
 writeColumns('image', "https://cdn.ssref.net/req/202311071/tlogo/bbr/NBA-2024.png")
+
+st.markdown("---")
 
 st.header("Hockey")
 st.text("Currently, this dashboard only has information about the NHL.")
 writeColumns('image', "https://cdn.ssref.net/req/202311071/tlogo/hr/NHL-2024.png")
+
+st.markdown("---")
 
 st.write("This dashboard is an open source tool, so check out the code at our repository [here](https://github.com/vnasserb/SportsDashboard)")
