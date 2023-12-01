08988cee20d659017d160d85b2b21fa5338ff118
vnasserb <83405997+vnasserb@users.noreply.github.com>
2023-11-30 21:49:14 -0300

    Update: Add more leagues

--- Streamlit/1_HomePage.py
+++ Streamlit/1_HomePage.py
@@ -23,11 +23,21 @@ st.write("**Use this cool dashboard to check information about many sports**")
 st.write("**Currently, we support soccer, basketball and Ice Hockey**")
 
 st.header("Soccer")
+st.text("This dashboard has information from many leagues around the world. Some of them are listed here.")
 writeColumns('image', 'https://cdn.ssref.net/req/202311071/tlogo/fb/9.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/12.png',
-            'https://cdn.ssref.net/req/202311071/tlogo/fb/20.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/24.png')
+            'https://cdn.ssref.net/req/202311071/tlogo/fb/20.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/11.png', 
+             'https://cdn.ssref.net/req/202311071/tlogo/fb/13.png')
+
+writeColumns('image', 'https://cdn.ssref.net/req/202311071/tlogo/fb/24.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/21.png',
+            'https://cdn.ssref.net/req/202311071/tlogo/fb/45.png', 'https://cdn.ssref.net/req/202311071/tlogo/fb/31.png',
+            'https://cdn.ssref.net/req/202311071/tlogo/fb/22.png')
 
 st.header("Basketball")
+st.text("Currently, this dashboard only has information about the NBA. More leagues will be available in the future.")
 writeColumns('image', "https://cdn.ssref.net/req/202311071/tlogo/bbr/NBA-2024.png")
 
 st.header("Hockey")
+st.text("Currently, this dashboard only has information about the NHL.")
 writeColumns('image', "https://cdn.ssref.net/req/202311071/tlogo/hr/NHL-2024.png")
+
+st.text("This dashboard is an open source tool, so check out the code at our repository [here](https://github.com/vnasserb/SportsDashboard)")
